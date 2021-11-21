0f402eefbff8f8d82b79ec6c667a6ab9aa9e7e09
Marvin <marvinvilaysack@gmail.com>
2021-11-21 17:10:01 -0600

    finish back button and confirm button

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import tw from "tailwind-styled-components"
 import Map from './components/Map'
-
+import Link from 'next/link'
 
 
 
@@ -26,10 +26,14 @@ export default function Home() {
         {/* ActionButtons */}
 
         <ActionButtons> 
+
+          {/* importing this Link from next/link */}
+            <Link href="search">
             <ActionButton>
               {/* self closing tag */}
                   <ActionButtonImage src="https://i.ibb.co/cyvcpfF/uberx.png"/>
               Ride</ActionButton>
+            </Link>
             <ActionButton>
                  <ActionButtonImage src="https://i.ibb.co/n776JLm/bike.pngg"/> 
               Wheels</ActionButton>
